6bd71e958d56b62acc0d979d843680ec27e162d1
Felipe Carvalho <felipe@autonifai.com>
2024-09-29 18:56:26 +0100

    Externaliza la base URL. Navega hasta el listado de posiciones usando atributos data-*. Comprueba que cada candidato esta en la posicion adecuada del proceso.

--- cypress/e2e/cambio-fase.cy.js
+++ cypress/e2e/cambio-fase.cy.js
@@ -4,11 +4,23 @@ describe('LTI Homepage Navigation', () => {
     cy.get('h1').contains('Dashboard del Reclutador')
   })
 
-  it('should navigate to the Positions page, click on the first "Ver proceso" button, and verify the URL', () => {
+  it('should navigate to the Positions page, click on the first "Ver proceso" button, verify the URL, and check the interview stages', () => {
     cy.visit('/') // Usa la URL base definida en la configuración
     cy.get('[data-cy="link-to-positions"]').click()
     cy.url().should('include', '/positions')
     cy.get('[data-cy="view-process-button"]').first().click()
     cy.url().should('include', '/positions/')
+    cy.get('.card-header').contains('Initial Screening')
+    cy.get('.card-header').contains('Technical Interview')
+    cy.get('.card-header').contains('Manager Interview')
+
+    // Verificar que los candidatos están en la fase correcta
+    cy.get('.card-header:contains("Initial Screening")').parent().within(() => {
+      cy.get('.card-body').contains('Carlos García')
+    })
+    cy.get('.card-header:contains("Technical Interview")').parent().within(() => {
+      cy.get('.card-body').contains('John Doe')
+      cy.get('.card-body').contains('Jane Smith')
+    })
   })
 })
